0761e7fe6b76edefc24e91ef5f51a2794078aeef
DerHyper <maxrichter15@web.de>
2025-08-01 19:20:52 +0200

    Add countdown logic

---
+++ Swing and Jump/Assets/_Scripts/Manager/CountDownManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class CountDownManager
+{
+    [SerializeField]
+    private GameObject countdownPrefab;
+    [SerializeField]
+    private Transform NumberPosition;
+    private FixedTimer _timer = new();
+    
+    private bool _timerHasStarted = false;
+    private bool _countHasStarted = false;
+
+    float countDownStartTimer = 10f;
+
+    float timeScineLastCountdownUpdate = 0f;
+    List<int> countDownNumbers = new List<int> { 5, 4, 3, 2, 1, 0 };
+    int current = 0;
+
+    private void Update()
+    {
+        if (!_timerHasStarted)
+        {
+            return;
+        }
+
+        _timer.Update();
+        float time = _timer.GetTime();
+        if (time > countDownStartTimer && !_countHasStarted)
+        {
+            _countHasStarted = true;
+
+        }
+
+        if (_countHasStarted && timeScineLastCountdownUpdate >= 1f)
+        {
+            timeScineLastCountdownUpdate = 0f;
+            showCountDownNumber(countDownNumbers[current]);
+            current++;
+        }
+    }
+
+    private void showCountDownNumber(int number) {
+        GameObject numberObj = GameObject.Instantiate(countdownPrefab, NumberPosition.position, NumberPosition.rotation, NumberPosition);
+        numberObj.GetComponent<CountDownNumber>().Init(number);
+    }
+}
---
+++ Swing and Jump/Assets/_Scripts/UI/CountDownNumber.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CountDownNumber : MonoBehaviour
+{
+    public void Init(int number)
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
