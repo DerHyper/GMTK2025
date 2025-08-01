a588bc0e8dd64d66623ed416b0b71e8972ce9243
DerHyper <maxrichter15@web.de>
2025-08-02 01:12:11 +0200

    Fix flying, now not using gravits

--- Swing and Jump/Assets/_Scripts/Units/GirlFlyingStart.cs
+++ Swing and Jump/Assets/_Scripts/Units/GirlFlyingStart.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class GirlFlyingStart : MonoBehaviour
 {
-    public Vector3 StartDirection = new(-1,1);
-    public Vector3 StartRotation = new(0,0,1);
-    public float StartForce = 10;
-    void Start()
-    {
-        GetComponent<Rigidbody>().AddForce(StartDirection * StartForce, ForceMode.Impulse);
-        GetComponent<Rigidbody>().AddTorque(StartRotation, ForceMode.Impulse);
+    public Vector3 direction = new(-1, 1, 0);
+    public Vector3 torque = new(0, 0, 1);
+    public float directionForce = 10;
+    
+    private void Update() {
+        Vector3 currentPos = gameObject.transform.position;
+        Vector3 nextPos = currentPos + direction * Time.deltaTime * directionForce;
+
+        Vector3 currentRot = gameObject.transform.rotation.eulerAngles;
+        Vector3 nextRot = currentRot + torque * Time.deltaTime;
+
+        gameObject.transform.SetPositionAndRotation(nextPos, Quaternion.Euler(nextRot));
     }
 }
