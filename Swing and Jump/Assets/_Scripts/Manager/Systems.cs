1837259cb7d54e99ae5eeb56efcbce09577a66e8
DerHyper <maxrichter15@web.de>
2025-08-31 19:26:20 +0200

    Fix Systems instantiating multible times

---
+++ Swing and Jump/Assets/_Scripts/Manager/Systems.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Systems : MonoBehaviour
+{
+    public static Systems Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
